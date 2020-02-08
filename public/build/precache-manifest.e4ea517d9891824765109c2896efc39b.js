0b7abc1568f7e94df5360a0ebd4bf1266fe5750a
tansawit <sawit.tr@gmail.com>
2020-02-08 16:37:23 +0700

    added: build file to git

---
+++ public/build/precache-manifest.e4ea517d9891824765109c2896efc39b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e8de93bbec1e3a43be739f3fa2b5e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "090861c115bdfd901929",
+    "url": "/static/js/2.d6259329.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.d6259329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523409c293a6b95261dc",
+    "url": "/static/js/main.7d3182d5.chunk.js"
+  },
+  {
+    "revision": "dfdc5870b5b9e5c7a902",
+    "url": "/static/js/runtime-main.8bc78da6.js"
+  }
+]);
